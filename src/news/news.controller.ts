c631514e3d10aee4bd462c7bdfb315327e709068
Ariel Barrios <ariel29111978@hotmail.com>
2023-12-21 22:14:06 -0300

    Agrega la conección con news api

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { NewsService } from './news.service';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Get()
+  async news() {
+    const { data } = await this.newsService.getNews();
+
+    return data;
+  }
+}
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,4 +1,17 @@
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  constructor(private readonly httpService: HttpService) {}
+
+  getNews(): Promise<AxiosResponse<any, any>> {
+    return this.httpService.axiosRef.get(
+      'https://newsapi.org/v2/everything?q=finanzas',
+      {
+        headers: { 'X-Api-Key': process.env.API_KEY },
+      },
+    );
+  }
+}
