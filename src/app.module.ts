70794a2146c6b3d0bf350d632633b6e24e42aa2a
Ariel Barrios <ariel29111978@hotmail.com>
2024-01-04 18:39:53 -0300

    Cambia la configuración del CacheModule para utilizar Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,21 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    CacheModule.register({ isGlobal: true }),
+    // CacheModule.register({ isGlobal: true }),
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      store: redisStore,
+      socket: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
     NewsModule,
   ],
   controllers: [AppController],
